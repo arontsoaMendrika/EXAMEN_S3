53f5ffac1b427d052815540dc5a389acc6c9a3fe
mandresy <mandresy128@gmail.com>
2026-02-16 14:52:16 +0300

    14:52

--- app/config/routes.php
+++ app/config/routes.php
@@ -1,5 +1,6 @@
 <?php
 
+use app\controllers\ApiDonsController;
 use app\controllers\ApiExampleController;
 use app\middlewares\SecurityHeadersMiddleware;
 use flight\Engine;
@@ -25,6 +26,13 @@ $router->group('', function(Router $router) use ($app) {
 		$router->get('/users', [ ApiExampleController::class, 'getUsers' ]);
 		$router->get('/users/@id:[0-9]', [ ApiExampleController::class, 'getUser' ]);
 		$router->post('/users/@id:[0-9]', [ ApiExampleController::class, 'updateUser' ]);
+
+		// Dons CRUD
+		$router->get('/dons', [ ApiDonsController::class, 'getDons' ]);
+		$router->get('/dons/@id:[0-9]', [ ApiDonsController::class, 'getDon' ]);
+		$router->post('/dons', [ ApiDonsController::class, 'createDon' ]);
+		$router->put('/dons/@id:[0-9]', [ ApiDonsController::class, 'updateDon' ]);
+		$router->delete('/dons/@id:[0-9]', [ ApiDonsController::class, 'deleteDon' ]);
 	});
 	
 }, [ SecurityHeadersMiddleware::class ]);
