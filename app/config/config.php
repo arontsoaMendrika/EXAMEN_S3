f6a70900ee8bb59d6ba82c7fc6ea9a628c53a880
RAVELOMANANTSOA Ny Sanda Itokiana <itokianara3@gmail.com>
2026-02-16 15:16:44 +0300

    Modifications config.php pour besoins et connexion MySQL

--- app/config/config.php
+++ app/config/config.php
@@ -66,12 +66,8 @@ return [
 	 *         Database Settings          *
 	 **************************************/
 	'database' => [
-<<<<<<< HEAD
 		// MySQL Example:
 		'host'     => '127.0.0.1',      // Database host (e.g., 'localhost', 'db.example.com')
-=======
-				'host'     => 'localhost',      // Database host (e.g., 'localhost', 'db.example.com')
->>>>>>> 53f5ffac1b427d052815540dc5a389acc6c9a3fe
 		'dbname'   => 'cyclone',   // Database name (e.g., 'flightphp')
 		'user'     => 'root',  // Database user (e.g., 'root')
 		'password' => '',  // Database password (never commit real passwords)
